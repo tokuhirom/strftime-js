a3a5a0b5a9b6ec6e17c1b7d675562838257eb9f0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-12-03 17:25:34 +0900

    add semicolons

--- strftime.js
+++ strftime.js
@@ -38,24 +38,24 @@ var locales = {
 };
 
 var formats = {
-		A: function (d, locale) { return locales[locale].A[d.getDay()] },
-		a: function (d, locale) { return locales[locale].a[d.getDay()] },
-		B: function (d, locale) { return locales[locale].B[d.getMonth()] },
-		b: function (d, locale) { return locales[locale].b[d.getMonth()] },
+		A: function (d, locale) { return locales[locale].A[d.getDay()]; },
+		a: function (d, locale) { return locales[locale].a[d.getDay()]; },
+		B: function (d, locale) { return locales[locale].B[d.getMonth()]; },
+		b: function (d, locale) { return locales[locale].b[d.getMonth()]; },
 		C: function (d) { return Math.floor(d.getFullYear()/100); },
-		c: function (d) { return d.toString() },
+		c: function (d) { return d.toString(); },
 		D: function (d) {
 				return formats.m(d) + '/' +
 					formats.d(d) + '/' + formats.y(d);
 			},
-		d: function (d) { return pad(d.getDate(), 2,'0') },
-		e: function (d) { return pad(d.getDate(), 2,' ') },
+		d: function (d) { return pad(d.getDate(), 2,'0'); },
+		e: function (d) { return pad(d.getDate(), 2,' '); },
 		F: function (d) {
 				return formats.Y(d) + '-' + formats.m(d) + '-' +
 					formats.d(d);
 			},
-		H: function (d) { return pad(d.getHours(), 2,'0') },
-		I: function (d) { return pad((d.getHours() % 12 || 12), 2) },
+		H: function (d) { return pad(d.getHours(), 2,'0'); },
+		I: function (d) { return pad((d.getHours() % 12 || 12), 2); },
         /*
 		j: function (d) {
 				var t = d.getDate();
@@ -69,41 +69,41 @@ var formats = {
 				return t.pad(3,'0');
 			},
             */
-		k: function (d) { return pad(d.getHours(), 2,' ') },
-		l: function (d) { return pad((d.getHours() % 12 || 12), 2,' ') },
-		M: function (d) { return pad(d.getMinutes(), 2,'0') },
-		m: function (d) { return pad((d.getMonth()+1), 2,'0') },
-		n: function (d) { return "\n" },
-		p: function (d) { return (d.getHours() > 11) ? 'PM' : 'AM' },
-		R: function (d) { return formats.H(d) + ':' + formats.M(d) },
+		k: function (d) { return pad(d.getHours(), 2,' '); },
+		l: function (d) { return pad((d.getHours() % 12 || 12), 2,' '); },
+		M: function (d) { return pad(d.getMinutes(), 2,'0'); },
+		m: function (d) { return pad((d.getMonth()+1), 2,'0'); },
+		n: function (d) { return "\n"; },
+		p: function (d) { return (d.getHours() > 11) ? 'PM' : 'AM'; },
+		R: function (d) { return formats.H(d) + ':' + formats.M(d); },
 		r: function (d) {
 				return formats.I(d) + ':' + formats.M(d) + ':' +
 					formats.S(d) + ' ' + formats.p(d);
 			},
-		S: function (d) { return pad(d.getSeconds(), 2,'0') },
-		s: function (d) { return Math.floor(d.getTime()/1000) },
+		S: function (d) { return pad(d.getSeconds(), 2,'0'); },
+		s: function (d) { return Math.floor(d.getTime()/1000); },
 		T: function (d) {
 				return formats.H(d) + ':' + formats.M(d) + ':' +
 					formats.S(d);
 			},
-		t: function (d) { return "\t" },
-/*		U: function (d) { return false }, */
-		u: function (d) { return(d.getDay() || 7) },
-/*		V: function (d) { return false }, */
+		t: function (d) { return "\t"; },
+/*		U: function (d) { return false; }, */
+		u: function (d) { return(d.getDay() || 7); },
+/*		V: function (d) { return false; }, */
 		v: function (d) {
 				return formats.e(d) + '-' + formats.b(d) + '-' +
 					formats.Y(d);
 			},
-/*		W: function (d) { return false }, */
-		w: function (d) { return d.getDay() },
-		X: function (d) { return d.toTimeString() }, // wrong?
-		x: function (d) { return d.toDateString() }, // wrong?
-		Y: function (d) { return d.getFullYear() },
-		y: function (d) { return pad((d.getYear() % 100), 2) },
+/*		W: function (d) { return false; }, */
+		w: function (d) { return d.getDay(); },
+		X: function (d) { return d.toTimeString(); }, // wrong?
+		x: function (d) { return d.toDateString(); }, // wrong?
+		Y: function (d) { return d.getFullYear(); },
+		y: function (d) { return pad((d.getYear() % 100), 2); },
 //		Z: function (d) { return d.toString().match(/\((.+)\)$/)[1]; },
-//		z: function (d) { return d.getTimezoneOffset() }, // wrong
+//		z: function (d) { return d.getTimezoneOffset(); }, // wrong
 //		z: function (d) { return d.toString().match(/\sGMT([+-]\d+)/)[1]; },
-		'%': function (d) { return '%' }
+		'%': function (d) { return '%'; }
 	};
 
 formats['+'] = formats.c;
@@ -113,7 +113,7 @@ Date.prototype.strftime =
 	function (fmt, locale) {
 		var r = '';
 		var n = 0;
-        if (!locale) { locale = 'en' }
+        if (!locale) { locale = 'en'; }
 		while(n < fmt.length) {
 			var c = fmt.substring(n, n+1);
 			if (c == '%') {
